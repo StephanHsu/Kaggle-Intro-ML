ded3ed4385cc7d9a26d146950681ee9d51e9b32a
stephan lai hsu <stephan.lai.hsu@gmail.com>
2020-06-30 20:31:07 -0300

    Initial commit

---
+++ Code/01-analise-exploratoria.R
@@ -0,0 +1,87 @@
+library(tidyverse)
+
+# Leitura das bases
+adults <- readRDS('intro-ml-mestre-master/dados/adult.rds')
+adults %>% head() %>% View()
+
+# Avaliando a variável resposta
+adults %>% 
+  count(resposta) %>% 
+  mutate(freq = n / sum(n))
+
+# Criando uma variável resposta dummy
+adults$resp_dummy = adults$resposta == '>50K'
+
+# Análise Exploratória ----------------------------------------------------
+
+# Análise Univariada
+adults %>% 
+  summarytools::dfSummary() %>% 
+  summarytools::view()
+
+adults %>% 
+  skimr::skim() # Outro jeito de fazer
+
+# Análise Bivariada
+
+# Categóricas
+vars_cat <- adults %>% select_if(is.character) %>% names() 
+vars_cat <- c(vars_cat, 'resp_dummy')
+ 
+# Quantidade de categorias
+adults[vars_cat] %>% 
+  map_df(n_distinct)
+
+# Cruzando com a resposta
+tab_cat <- adults[vars_cat] %>% 
+  gather(var, categ, -resp_dummy) %>% 
+  group_by(var, categ) %>% 
+  summarise(tx_resp = mean(resp_dummy),
+            qtd = n()) %>% 
+  arrange(var, -tx_resp)
+
+# Alguns gráficos
+graf_cat <- function(nome_var){
+  if(!(nome_var %in% c('resp_dummy','resposta'))){
+    print(tab_cat %>%
+      filter(var == nome_var) %>%
+      ggplot() +
+      geom_col(aes(x = reorder(categ, -tx_resp), y = tx_resp)) +
+      labs(x = nome_var))
+  }
+}
+
+for(var in vars_cat) graf_cat(var)
+
+
+# Numéricas
+vars_num <- adults %>% select_if(is.numeric) %>% names() 
+vars_num <- c(vars_num, 'resposta')
+
+# Missings
+adults[vars_num] %>% 
+  map_df(function(x) is.na(x) %>% sum())
+
+# Correlação entre as variáveis
+adults[vars_num] %>% select(-resposta, -id) %>% 
+  cor()
+
+# Calculando a média de cada variável pela resposta
+adults[vars_num] %>% 
+  group_by(resposta) %>% 
+  summarise_all(list(mean = mean))
+
+# Alguns gráficos
+graf <- function(nome_var){
+  
+  if(!(var %in% c('resposta','id'))){
+    print(adults[vars_num] %>% 
+      ggplot() +
+      geom_density(aes(x = get(nome_var), fill = resposta, alpha = .8)) +
+      labs(x = nome_var))
+}}
+
+for(var in vars_num) graf(var)
+
+# Outro jeito
+# GGally::ggpairs(adults[vars_num])
---
+++ Code/02-modelagem.R
@@ -0,0 +1,103 @@
+library(tidyverse)
+library(recipes)
+library(rsample)
+
+# Leitura da base
+adults <- readRDS('intro-ml-mestre-master/dados/adult.rds')
+adults$resposta <- factor(adults$resposta)
+
+# Separando a base em treino e teste
+set.seed(1)
+adults_split <- initial_split(adults, prop = 0.8)
+
+# Pegando as bases de treino e teste
+adults_train <- training(adults_split)
+adults_test <- testing(adults_split)
+
+# Base para cross-validation
+# adults_cv <- vfold_cv(adults_train, v = 5)
+
+# Feature Engine ----------------------------------------------------------
+adults_recipe <- recipe(resposta ~ ., data = adults_train) %>% 
+  step_rm(education, native_country, capital_loss, fnlwgt, id) %>% 
+  step_center(all_predictors(), -all_nominal()) %>% 
+  step_unknown(all_predictors(), -all_numeric()) %>%
+  step_dummy(all_predictors(), -all_numeric()) %>% 
+  # step_log(capital_gain) %>% 
+  prep()
+
+adults_train <- adults_recipe %>% juice()
+
+
+# Modelagem ---------------------------------------------------------------
+
+# Especificando o modelo de regressão logística
+modelo_log_reg <- logistic_reg() %>% 
+  set_engine("glm") %>% 
+  set_mode("classification")
+
+# Criando um workflow
+# log_reg_workflow <- workflow() %>% 
+#   add_recipe(adults_recipe) %>% 
+#   add_model(modelo_log_reg)
+
+# Ajustando o modelo com cross-validation
+adults_model <- modelo_log_reg %>% 
+  fit(resposta ~ ., data = adults_train)
+
+# Incluindo as previsões na base
+adults_train_pred <- adults_train %>% 
+  mutate(
+    pred = predict(adults_model, new_data = .)$.pred_class,
+    prob = predict(adults_model, new_data = ., type = "prob")$`.pred_>50K`
+  )
+
+# Métricas de ajuste do modelo
+multimetric <- metric_set(accuracy, 
+                          kap,
+                          bal_accuracy, 
+                          sens, 
+                          yardstick::spec, 
+                          precision, 
+                          recall, 
+                          ppv, 
+                          npv)
+
+adults_train_pred %>% multimetric(truth = resposta, estimate = pred)
+adults_train_pred %>% roc_auc(truth = resposta, prob)
+
+# aplicando na base teste
+adults_test <- adults_recipe %>% bake(adults_test)
+adults_test_pred <- adults_test %>% 
+  mutate(
+    pred = predict(adults_model, new_data = .)$.pred_class,
+    prob = predict(adults_model, new_data = ., type = "prob")$`.pred_>50K`
+  )
+
+adults_test_pred %>% multimetric(truth = resposta, estimate = pred)
+adults_test_pred %>% roc_auc(truth = resposta, prob)
+
+
+# Salvando workflow e modelo
+saveRDS(adults_model, "adults_model.RDS")
+
+
+# Fazendo previsão --------------------------------------------------------
+
+# Lendo a base para previsão
+submit <- readRDS("intro-ml-mestre-master/dados/dados_kaggle/adult_val.rds")
+id <- submit$id
+resposta <- submit$resposta
+submit <- adults_recipe %>% bake(submit %>% select(-resposta))
+submit_pred <- submit %>% 
+  mutate(
+    pred = predict(adults_model, new_data = .)$.pred_class,
+    prob = predict(adults_model, new_data = ., type = "prob")$`.pred_>50K`
+  )
+
+submit_final <- tibble(id = id,
+       more_than_50K = submit_pred$prob,
+       resposta_pred = factor(submit_pred$pred),
+       resposta = factor(resposta))
+
+submit_final %>% accuracy(truth = resposta, estimate = resposta_pred)
